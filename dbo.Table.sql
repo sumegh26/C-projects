9745f5371870c8db954856e7391fa2eb4fe3c776
sumegh thale <40857938+sumegh26@users.noreply.github.com>
2020-12-02 23:49:17 +0530

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[student] (
+    [ROLLNO]  INT        NOT NULL,
+    [NAME]     CHAR(100) NULL,
+    [DATEOFBIRTH]      NCHAR (100) NULL,
+    [COURSEID] INT        NULL,
+    PRIMARY KEY CLUSTERED ([ROLLNO] ASC), 
+    CONSTRAINT [FK_student_course] FOREIGN KEY ([courseID]) REFERENCES [course]([COURSEID])
+);
+
---
+++ dbo.course.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[course]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [cName] NCHAR(10) NULL, 
+    [duration(month)] INT NULL, 
+    [fees] FLOAT NULL, 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([courseID]), 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([ID])
+)
